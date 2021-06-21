000eed87831fa3d27e9fa70cef1323c73b8c0ca5
刘星雨 <charlotteliu12x@gmail.com>
2021-06-21 14:18:37 +0800

    update rankdata

--- scipy/stats/rank_data_pythran.py
+++ scipy/stats/rank_data_pythran.py
@@ -1,22 +1,23 @@
 import numpy as np
 
-
+#pythran export rankdata_pythran(float[:])
+#pythran export rankdata_pythran(float[:], string)
 #pythran export rankdata_pythran(float[:], string, None)
 #pythran export rankdata_pythran(float[:], string, int)
 def rankdata_pythran(a, method='average', axis=None):
     if method not in ('average', 'min', 'max', 'dense', 'ordinal'):
         raise ValueError('unknown method "{0}"'.format(method))
 
-    # if axis is not None:
-    #     a = np.asarray(a)
-    #     if a.size == 0:
-    #         # The return values of `normalize_axis_index` are ignored.  The
-    #         # call validates `axis`, even though we won't use it.
-    #         # use scipy._lib._util._normalize_axis_index when available
-    #         np.core.multiarray.normalize_axis_index(axis, a.ndim)
-    #         dt = np.float64 if method == 'average' else np.int_
-    #         return np.empty(a.shape, dtype=dt)
-    #     return np.apply_along_axis(rankdata_pythran, axis, a, method)
+    if axis is not None:
+        a = np.asarray(a)
+        if a.size == 0:
+            # The return values of `normalize_axis_index` are ignored.  The
+            # call validates `axis`, even though we won't use it.
+            # use scipy._lib._util._normalize_axis_index when available
+            np.core.multiarray.normalize_axis_index(axis, a.ndim)
+            dt = np.float64 if method == 'average' else np.int_
+            return np.empty(a.shape, dtype=dt)
+        return np.apply_along_axis(rankdata_pythran, axis, a, method)
 
     arr = np.ravel(np.asarray(a))
     algo = 'mergesort' if method == 'ordinal' else 'quicksort'
