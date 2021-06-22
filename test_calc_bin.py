df214ad8c7bf00e31ce557c655183f4c20c76f3e
刘星雨 <charlotteliu12x@gmail.com>
2021-06-22 22:42:44 +0800

    add test calc bin

--- test_calc_bin.py
+++ test_calc_bin.py
@@ -24,7 +24,7 @@ f_median = lambda: stats.binned_statistic_dd(
     [inp[0], inp[1]], inp[2], statistic='median',
     bins=[subbin_x_edges, subbin_y_edges])
 
-print(timeit(f_min, number=100)) 
-print(timeit(f_max, number=100)) 
-print(timeit(f_std, number=100)) 
-print(timeit(f_median, number=100)) 
+print(timeit(f_min, number=100)) # 1.3672322449999998 vs 0.40702188699999997(python vs pythran)
+print(timeit(f_max, number=100)) # 1.346275941 vs 0.496832677
+print(timeit(f_std, number=100)) # 0.47545129600000013 vs 0.3890081329999999
+print(timeit(f_median, number=100)) # 5.6123907740000005 vs 0.4219133049999999
