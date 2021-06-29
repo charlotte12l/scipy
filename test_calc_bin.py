d31fdcbc9d8f0b5a575b0588a930d113fe29cdbe
刘星雨 <charlotteliu12x@gmail.com>
2021-06-29 22:03:54 +0800

    update test

--- test_calc_bin.py
+++ test_calc_bin.py
@@ -11,7 +11,7 @@ subbin_y_edges = np.arange(0, 200, dtype=np.float64)
 stats.binned_statistic_dd(
     [inp[0], inp[1]], inp[2], statistic='count',
     bins=[subbin_x_edges, subbin_y_edges])
-'''
+
 f_min = lambda: stats.binned_statistic_dd(
     [inp[0], inp[1]], inp[2], statistic='min',
     bins=[subbin_x_edges, subbin_y_edges])
@@ -32,10 +32,3 @@ print(timeit(f_min, number=100)) # 1.3672322449999998 vs 0.40702188699999997(pyt
 print(timeit(f_max, number=100)) # 1.346275941 vs 0.496832677
 print(timeit(f_std, number=100)) # 0.47545129600000013 vs 0.3890081329999999
 print(timeit(f_median, number=100)) # 5.6123907740000005 vs 0.4219133049999999
-'''
-
-import numpy as np
-result = np.zeros((2,3))
-flatcount = np.array([1,2,3])
-result[:, 1] = flatcount[1]
-print(result)
